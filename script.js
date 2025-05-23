eaf24811779287388f36d50a97c63a39c542b9ea
Maddubtt <maddubtt25@gmail.com>
2025-05-23 04:35:26 -0300

    Ajustes no Form e adição do carrossel

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+// BOTÕES
+
 const btnContato = document.getElementById('btn-contato');
 const btnServicos = document.getElementById('btn-servicos');
 const btnCadastro = document.getElementById('btn-cadastro');
+const btnPessoa = document.getElementById('btn-pessoa');
+const btnOng = document.getElementById('btn-ong');
 
 btnContato.addEventListener('click', () => {
     window.location.href = 'contato.html';
@@ -13,3 +17,22 @@ btnServicos.addEventListener('click', () => {
 btnCadastro.addEventListener('click', () => {
     window.location.href = 'form.html';
 });
+
+btnPessoa.addEventListener('click', () => {
+    window.location.href = 'form_v.html';
+});
+
+btnOng.addEventListener('click', () => {
+    window.location.href = 'form_ong.html';
+});
+
+// CARROSSEL
+
+const track = document.querySelector('.carousel-track');
+let currentIndex = 0;
+const totalImages = document.querySelectorAll('.carousel-track img').length;
+
+setInterval(() => {
+  currentIndex = (currentIndex + 1) % totalImages;
+  track.style.transform = `translateX(-${currentIndex * 100}%)`;
+}, 3000);
